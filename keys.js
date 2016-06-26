45cf9450f011ca2946a53bf505e3da3b6b843e5f
ftarlaci <ftarlaci@utexas.edu>
2016-06-26 13:35:22 -0500

    keys created

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'en5tmdKGjFtmuM5d2rdyv2gzk',
+  consumer_secret: 'a569OBLujVFJILmKOuVB6D6SrJO014wMAJwZ7Ya0Srn3NmJcER',
+  access_token_key: '91510093-wThtxuZ1KLuPsbaz5DFmzY4AXLpBHiKWdJU17iaYM',
+  access_token_secret: 'iGpzCL4O8ptqJOY5PnfSlbKfOSoQssWTKiID867GmAuAg',
+}
---
+++ liri.js
@@ -0,0 +1,9 @@
+// fs = require('fs');
+// fs.readFile(./keys.js)
+var myKeys = require('./keys.js');
+console.log('keys.js')
+
+exports.myKeys = function() {
+	console.log('keys.js')
+
+};
